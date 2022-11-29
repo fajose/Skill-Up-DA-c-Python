c20c6922fff52c0f846dbeb1092ac14190e757b8
GastonOrphant <gaston.orphant@hotmail.com>
2022-11-29 17:55:53 -0300

    Grupo C

---
+++ include/GrupoC_jujuy_universidad.sql
@@ -0,0 +1,12 @@
+SELECT university, 
+	   career, 
+	   to_date(inscription_date, 'YYYY/MM/DD') AS inscription_date, 
+	   nombre AS first_name, 
+	   NULL AS last_name, 
+	   sexo AS gender, 
+	   birth_date AS age, 
+	   NULL AS postal_code, 
+	   location, 
+	   email 
+FROM jujuy_utn
+WHERE (university = 'universidad nacional de jujuy') AND (inscription_date BETWEEN '2020-09-01' and '2021-02-01');
