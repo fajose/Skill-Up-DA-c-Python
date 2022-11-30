52c13d85825b43a801c0d592438d603a27b39f64
Leandro Serra <serraleandro29@gmail.com>
2022-11-29 21:40:53 -0300

    Added dags with EmptyOperators for university groups E and F

---
+++ dags/GEUAInteramericana_dag_etl.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from datetime import timedelta, datetime
+
+default_args = {
+    'retries': '5',
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id = 'UAInteramericana_dag_etl',
+    default_args= default_args,
+    schedule= '@hourly',
+    start_date= datetime(2022, 11, 28)
+) as dag:
+    extract = EmptyOperator(task_id='Extract') #pythonOperator > postgresOperator/ postgresHook ?
+    transform = EmptyOperator(task_id='Transform') #pythonOperator?
+    load = EmptyOperator(task_id='load') #pythonOperator > s3Hook
+    extract >> transform >> load
