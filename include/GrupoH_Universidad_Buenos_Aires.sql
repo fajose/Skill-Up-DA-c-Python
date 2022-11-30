af6425756460f6d852a8e088f952f478b476a223
fajose <70110590+fajose@users.noreply.github.com>
2022-11-30 08:45:05 -0500

    Fixed localidades

--- include/GrupoH_Universidad_Buenos_Aires.sql
+++ include/GrupoH_Universidad_Buenos_Aires.sql
@@ -8,9 +8,8 @@ SELECT
     fechas_nacimiento as birth_date,
     NULL as age,
     codigos_postales as postal_code, 
-    l.localidad as location, 
+    NULL as location, 
     emails as email
-FROM uba_kenedy uk, localidad l 
-where cast(uk.codigos_postales as integer) = l.codigo_postal
+FROM uba_kenedy uk
 and universidades = 'universidad-de-buenos-aires' 
 and to_date(fechas_de_inscripcion, 'YY-Mon-DD') between to_date('01-09-2020','DD-MM-YYY') and to_date('01-02-2021','DD-MM-YYY');
