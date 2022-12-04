30d57ed8478a250a15d7f2f89bf901cbd51bf2c2
Kevin Marcos Agüi Manera <100862089+KevinMarcos26@users.noreply.github.com>
2022-12-04 14:49:08 -0300

    Update GrupoA_flores_universidad_dag_etl.py

--- dags/GrupoA_flores_universidad_dag_etl.py
+++ dags/GrupoA_flores_universidad_dag_etl.py
@@ -1,47 +1,46 @@
-import os
-import logging
-import csv
-import boto3
+from airflow import DAG
+from airflow.decorators import task
+from datetime import datetime, timedelta
+from helper_functions import logger_setup
+from helper_functions.utils import *
+from helper_functions.transforming import transformation
+from helper_functions.extracting import extraction
 import pandas as pd
-from pathlib import Path
-from datetime import date, datetime, timedelta
-from airflow.models import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.decorators import dag, task
 
 
-default_args = {
-    "owner": "Kevin Marcos Agui Manera",
-    "retries": 5,
-    "retry_delay": timedelta(minutes=1),
-}
+# Universidad
+university = 'GrupoA_flores_universidad'
 
-# Instantiate DAG
-with DAG(
-    dag_id="G1UNFlores_dag_etl",
-    start_date=datetime(2022, 12, 1),
-    max_active_runs=5,
-    schedule_interval="@hourly",
-    default_args=default_args,
-    catchup=False,
-) as dag:
+# Configuracion del logger
+logger = logger_setup.logger_creation(university)
 
-    with open("dags/sql/flores_universidad.sql", 'r') as myfile:
-        data = myfile.read()
-        print(data)
+# Definimos el DAG
+with DAG(f'{university}_dag_etl',
+         default_args=default_args,
+         catchup=False
+         ) as dag:      
     
+    # Extracción de Datos
     @task()
-    def Extraccion(**kwargs):
+    def extract():
+        logger.info('Inicio de proceso de extracción')
         try:
-            hook = PostgresHook(postgres_conn_id="alkemy_db")
-            df = hook.get_pandas_df(sql=data)
-            df.to_csv("dags/files/G1UNFlores.csv")
-            logging.info('Tarea de extraccion EXITOSA')
-        except:
-            logging.info('ERROR al extraer')
-            
-    Extraccion()
-            
+            extraction(university)
+            logger.info("Se creo el csv con la información de la universidad")
+
+        except Exception as e:
+            logger.error(e)
     
-     
+    # Transformación de Datos
+    @task()
+    def transform():
+        logger.info('Inicia proceso de transformación de los datos')
+
+        try:
+            transformation(university)
+            logger.info('Se creo archivo csv con la información transformada')
+            
+        except Exception as e:
+            logger.error(e)
+    
+    extract() >> transform()
