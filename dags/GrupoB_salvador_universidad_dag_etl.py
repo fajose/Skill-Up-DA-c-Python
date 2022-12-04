fe1a2838405d9145b80bfd7f3f2a2912c42f36b9
Kevin Marcos Agüi Manera <100862089+KevinMarcos26@users.noreply.github.com>
2022-12-04 14:59:48 -0300

    Update and rename G1UNSalvador_dag_etl.py to GrupoB_salvador_universidad_dag_etl.py

---
+++ dags/GrupoB_salvador_universidad_dag_etl.py
@@ -0,0 +1,47 @@
+from airflow import DAG
+from airflow.decorators import task
+
+from datetime import datetime, timedelta
+from helper_functions import logger_setup
+from helper_functions.utils import *
+from helper_functions.transforming import transformation
+from helper_functions.extracting import extraction
+import pandas as pd
+
+
+# Universidad
+university = 'GrupoB_salvador_universidad'
+
+# Configuracion del logger
+logger = logger_setup.logger_creation(university)
+
+# Definimos el DAG
+with DAG(f'{university}_dag_etl',
+         default_args=default_args,
+         catchup=False
+         ) as dag:      
+    
+    # Extracción de Datos
+    @task()
+    def extract():
+        logger.info('Inicio de proceso de extracción')
+        try:
+            extraction(university)
+            logger.info("Se creo el csv con la información de la universidad")
+
+        except Exception as e:
+            logger.error(e)
+    
+    # Transformación de Datos
+    @task()
+    def transform():
+        logger.info('Inicia proceso de transformación de los datos')
+
+        try:
+            transformation(university)
+            logger.info('Se creo archivo csv con la información transformada')
+            
+        except Exception as e:
+            logger.error(e)
+    
+    extract() >> transform()
