99d67018ecb6e3676bd67a473fbf346ae972b230
Leandro Serra <serraleandro29@gmail.com>
2022-12-06 14:21:09 -0300

    Added transformation

--- dags/GFUMoron_dag_etl.py
+++ dags/GFUMoron_dag_etl.py
@@ -1,10 +1,11 @@
+from datetime import timedelta, datetime
 import pandas as pd
 #ariflow imports
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from datetime import timedelta, datetime
+from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
 #helper functions imports
 from helper_functions.logger_setup import logger_creation
 from helper_functions.extracting import extraction
@@ -16,6 +17,16 @@ logger = logger_creation(university)
 
 # Connection with database
 POSTGRES_ID = "alkemy_db"
+S3_ID = "aws_s3_bucket"
+
+def calculateAge(birthDate:datetime):
+    today = datetime.today()
+    if today.month < birthDate.month:
+        return today.year - birthDate.year - 1
+    elif today.month == birthDate.month and today.day < birthDate.day:
+        return today.year - birthDate.year - 1
+    else: 
+        return today.year - birthDate.year
 
 def extract():
     try:
@@ -26,8 +37,38 @@ def extract():
         logger.error(err)
         raise
 
-def transformation():
-    pass
+def transform():
+    logger.info('Beggining transformation')
+    try:
+        df = pd.read_csv(f'./files/{university}_select.csv')
+        df.drop(df.columns[0], axis=1, inplace=True)
+
+        df['university'] = df['university'].str.replace('-', ' ').str.strip().str.lower()
+        df['career'] = df['career'].str.replace('-', ' ').str.strip().str.lower()
+        df['first_name'] = df['first_name'].str.replace('-', ' ').str.strip().str.lower()
+        df['email'] = df['email'].str.replace('-', '').str.strip().str.lower()
+        
+        df[['first_name', 'last_name']] = df['first_name'].str.split(" ", n = 1, expand=True)
+        
+        df['gender'] = df['gender'].map({'M': 'male', 'F': 'female'})
+
+        df['birth_date'] = pd.to_datetime(df['birth_date'], format='%d/%m/%Y')
+        df['inscription_date'] = pd.to_datetime(df['inscription_date'], format='%d/%m/%Y')
+
+        df['age'] = df['birth_date'].map(calculateAge)
+        df = df[df['age'].between(18, 100)]
+        df.drop('birth_date', axis=1, inplace=True)
+
+        dfCPostal = pd.read_csv('./assets/codigos_postales.csv')
+        dfCPostal.rename(columns={'localidad':'location', 'codigo_postal': 'postal_code'}, inplace=True)
+        dfCPostal['location'] = dfCPostal['location'].str.lower()
+        df['location'] = df['postal_code'].map(dfCPostal.set_index('postal_code')['location'])
+        
+        df.to_csv(f'./datasets/{university}_process.txt', sep='\t', index=False)
+    except Exception as err:
+        logger.error(err)
+        raise
+    logger.info('Transformation finished without errors')
 
 default_args = {
     'owner': 'Leandro Serra',
@@ -47,7 +88,7 @@ with DAG(
     )
     transform = PythonOperator(
         task_id='transfrom',
-        python_callable=transformation
+        python_callable=transform
     )
     load = EmptyOperator(task_id='load') #pythonOperator > s3Hook
     extract >> transform >> load
